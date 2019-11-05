045b4456784963485df8cfc2f9cd2128a9eaafc5
James Messinger <james.messinger@gmail.com>
2019-11-05 08:10:32 -0600

    Clarified the purpose of the `processFile()` method

--- src/process-file.ts
+++ src/process-file.ts
@@ -4,8 +4,19 @@ import { createFileUrl } from "./create-file";
 import { NormalizedConfig } from "./normalize-config";
 
 /**
- * Populates the contents of filesystem files, regardless of whether they were created by this plugin's
- * `read()` or `watch()` methods, or by another plugin.
+ * If another plugin (other than the Filesystem Source plugin) creates filesystem files within this
+ * plugin's source path, then this plugin will automatically read the file contents from disk and
+ * populate the `file.contents` property. This is mostly intended to allow other plugins' `watch()`
+ * methods to trigger change events for this plugin's files.
+ *
+ * For example, assume that there's another plugin that adds layout support for HTML files. The
+ * layout plugin would watch for changes to layout templates (which might be handlebars, nunjucks,
+ * etc.), but NOT to the HTML files. Instead, the HTML files are read and watched by the Filesystem
+ * Source plugin.  So whenever a layout template changes, the layout plugin will need to trigger
+ * change events for the template _as well as_ all HTML files, since they need to be re-built to
+ * include the new template changes. But the layout plugin isn't responsible for reading the
+ * _contents_ of the HTML files. That's the Filesystem Source plugin's responsibility. And that's
+ * what this `processFile()` method does.
  */
 export function processFile(config: NormalizedConfig): FileProcessor {
   let source = createFileUrl(config.path);
