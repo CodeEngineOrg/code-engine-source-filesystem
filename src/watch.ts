91792141656cc54c2cc2c3fc62ec3debdfd2f63d
James Messinger <james.messinger@gmail.com>
2019-11-18 06:27:13 -0600

    Refactored the watch functionality as an internal class

--- src/watch.ts
+++ src/watch.ts
@@ -1,8 +1,10 @@
 // tslint:disable: no-promise-as-boolean
-import { ChangedFile, Context } from "@code-engine/types";
+import { ChangedFile, Context, FileChange } from "@code-engine/types";
 import { IterableWriter } from "@code-engine/utils";
 import * as chokidar from "chokidar";
-import { posix } from "path";
+import { Stats } from "fs";
+import { join, posix } from "path";
+import { createChangedFile } from "./create-file";
 import { DirPathInfo, FilePathInfo, getPathInfo } from "./get-path-info";
 import { NormalizedConfig } from "./normalize-config";
 
@@ -10,24 +12,16 @@ import { NormalizedConfig } from "./normalize-config";
  * Watches the filesystem and yields any changes that are detected.
  */
 export function watch(config: NormalizedConfig) {
-  let watcher: chokidar.FSWatcher | undefined;
-  let writer: IterableWriter<ChangedFile> | undefined;
+  let watcher: Watcher | undefined;
 
   return {
     /**
      * Starts watching the filesystem for changes.
      */
     async startWatching(context: Context) {
-      writer = new IterableWriter<ChangedFile>();
       let path = await getPathInfo(config, context);
-      watcher = createWatcher(path, config);
-
-      watcher.on("xxxxxx", (file) => writer && writer.write(file));
-
-      // Propagate errors from Chokidar
-      watcher.on("error", (error) => writer && writer.throw(error));
-
-      return writer.iterable;
+      watcher = new Watcher(path, config, context);
+      return watcher.iterable;
     },
 
 
@@ -35,26 +29,106 @@ export function watch(config: NormalizedConfig) {
      * Stops watching the filesystem for changes.
      */
     async stopWatching() {
-      let promises = [];
-
       if (watcher) {
-        promises.push(watcher.close());
+        let promise = watcher.dispose();
         watcher = undefined;
+        await promise;
       }
-      if (writer) {
-        promises.push(writer.end());
-        writer = undefined;
-      }
-
-      await Promise.all(promises);
     }
   };
 }
 
+
+/**
+ * Internal class that wraps the filesystem watch functionality.
+ */
+class Watcher {
+  private chokidar: chokidar.FSWatcher;
+  private output: IterableWriter<ChangedFile>;
+  private config: NormalizedConfig;
+  private context: Context;
+  private dir: string;
+
+  public get iterable() {
+    return this.output.iterable;
+  }
+
+  public constructor(path: DirPathInfo | FilePathInfo, config: NormalizedConfig, context: Context) {
+    this.chokidar = createChokidar(path, config);
+    this.output = new IterableWriter<ChangedFile>();
+    this.config = config;
+    this.context = context;
+    this.dir = path.dir;
+
+    this.chokidar.on("add", this.newFile.bind(this));
+    this.chokidar.on("change", this.fileChanged.bind(this));
+    this.chokidar.on("unlink", this.fileDeleted.bind(this));
+    this.chokidar.on("addDir", this.newSubDirectory.bind(this));
+    this.chokidar.on("unlinkDir", this.subDirectoryDeleted.bind(this));
+    this.chokidar.on("error", this.error.bind(this));
+  }
+
+  /**
+   * Disposes the watcher. It cannot be used after this.
+   */
+  public async dispose() {
+    await Promise.all([
+      this.chokidar.close(),
+      this.output.end(),
+    ]);
+  }
+
+  /**
+   * A new file was created in the directory path.
+   */
+  private async newFile(path: string, stats: Stats) {
+    let file = createChangedFile(path, stats, FileChange.Created);
+
+    if (this.config.filter(file, this.context)) {
+      file.contents = await this.config.fs.promises.readFile(join(this.dir, path));
+      await this.output.write(file);
+    }
+  }
+
+  /**
+   * A file in the directory path was modified.
+   */
+  private async fileChanged(path: string, stats: Stats) {
+    return;
+  }
+
+  /**
+   * A file in the directory path was deleted.
+   */
+  private async fileDeleted(path: string, stats: Stats) {
+    return;
+  }
+
+  /**
+   * A new sub-directory has been created in the directory path.
+   */
+  private async newSubDirectory(path: string, stats: Stats) {
+    return;
+  }
+
+  /**
+   * A sub-directory in the directory path has beeen deleted.
+   */
+  private async subDirectoryDeleted(path: string, stats: Stats) {
+    return;
+  }
+
+  private async error(error: Error) {
+    // Re-throw the error into the iterable
+    await this.output.throw(error);
+  }
+}
+
+
 /**
  * Creates a Chokidar filesystem watcher.
  */
-function createWatcher(path: DirPathInfo | FilePathInfo, config: NormalizedConfig): chokidar.FSWatcher {
+function createChokidar(path: DirPathInfo | FilePathInfo, config: NormalizedConfig): chokidar.FSWatcher {
   let whatToWatch, disableGlobbing;
 
   if (typeof config.filterCriteria === "string") {
@@ -67,16 +141,15 @@ function createWatcher(path: DirPathInfo | FilePathInfo, config: NormalizedConfi
     disableGlobbing = true;
   }
 
-  let watcher = chokidar.watch(whatToWatch, {
+  return chokidar.watch(whatToWatch, {
     ignoreInitial: true,
     cwd: path.dir,
     disableGlobbing,
     alwaysStat: true,
     depth: config.depth,
   });
-
-  return watcher;
 }
+
 
 /**
  * Combines the specified directory path and glob pattern.
