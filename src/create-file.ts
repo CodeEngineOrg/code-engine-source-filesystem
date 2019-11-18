ce0d1dcb6725f49ee663ef9e3216c878f8210764
James Messinger <james.messinger@gmail.com>
2019-11-18 06:26:56 -0600

    Split the `createFile()` function into separate functions and exposed an additional `createChangedFile()`

--- src/create-file.ts
+++ src/create-file.ts
@@ -1,20 +1,39 @@
-import { Cloneable, File, FileMetadata } from "@code-engine/types";
-import { createFile as createCodeEngineFile } from "@code-engine/utils";
+import { ChangedFile, ChangedFileInfo, Cloneable, File, FileChange, FileInfo, FileMetadata } from "@code-engine/types";
+import { createChangedFile as createCodeEngineChangedFile, createFile as createCodeEngineFile } from "@code-engine/utils";
 import { Stats } from "fs";
 import { URL } from "url";
+
+/**
+ * Creates a CodeEngine `File` object from a node `Stats` object.
+ * @internal
+ */
+export function createFile(path: string, stats: Stats): File {
+  let info = createFileInfo(path, stats);
+  return createCodeEngineFile(info);
+}
+
+/**
+ * Creates a CodeEngine `ChangedFile` object from a node `Stats` object.
+ * @internal
+ */
+export function createChangedFile(path: string, stats: Stats, change: FileChange): ChangedFile {
+  let info = createFileInfo(path, stats) as ChangedFileInfo;
+  info.change = change;
+  return createCodeEngineChangedFile(info);
+}
 
 /**
  * Creates a CodeEngine `FileInfo` object from a node `Stats` object.
  * @internal
  */
-export function createFile(path: string, stats: Stats): File {
-  return createCodeEngineFile({
+function createFileInfo(path: string, stats: Stats, change?: FileChange): FileInfo {
+  return {
     path,
     source: createFileUrl(path),
     createdAt: stats.birthtime,
     modifiedAt: stats.mtime,
     metadata: createMetadata(stats),
-  });
+  };
 }
 
 /**
